8299bace18f8ef0fa2ba02bd4c97ddee2bd7b6ae
kischepazaryeri-ops <kischepazaryeri@gmail.com>
2026-05-10 14:54:11 +0300

    Update DM sending endpoint to use Instagram account ID

--- src/services/instagram.js
+++ src/services/instagram.js
@@ -3,12 +3,14 @@ const axios = require('axios');
 
 const BASE_URL = 'https://graph.facebook.com/v19.0';
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const INSTAGRAM_ACCOUNT_ID = process.env.INSTAGRAM_ACCOUNT_ID;
 
 // Kullanıcıya DM gönder
 async function sendDM(recipientId, message) {
   try {
     const response = await axios.post(
-      `${BASE_URL}/me/messages`,
+      `${BASE_URL}/${INSTAGRAM_ACCOUNT_ID}/messages`,
+
       {
         recipient: { id: recipientId },
         message: { text: message },
